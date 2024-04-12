679287c218bad59f37da9c8cc4019f159d7bda0b
LorincAkos <lorinc.akos13@gmail.com>
2024-04-12 16:54:03 +0200

    Sprites added

---
+++ 7days7nights_no_0/SpriteContainer.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection;
+
+namespace ZombieGameMovement
+{
+    static internal class SpriteContainer
+    {
+        static public readonly Image[] walkRight =
+        [
+            GetImageFromExecutingAssembly("Kuno_walk1.png"),
+            GetImageFromExecutingAssembly("Kuno_walk2.png"),
+            GetImageFromExecutingAssembly("Kuno_walk3.png"),
+            GetImageFromExecutingAssembly("Kuno_walk4.png"),
+        ];
+
+        static public readonly Image[] zombieWalkRight =
+        [
+            GetImageFromExecutingAssembly("Zom_walk1.png"),
+            GetImageFromExecutingAssembly("Zom_walk2.png"),
+            GetImageFromExecutingAssembly("Zom_walk3.png"),
+            GetImageFromExecutingAssembly("Zom_walk4.png"),
+            GetImageFromExecutingAssembly("Zom_walk5.png"),
+            GetImageFromExecutingAssembly("Zom_walk6.png"),
+            GetImageFromExecutingAssembly("Zom_walk7.png"),
+            GetImageFromExecutingAssembly("Zom_walk8.png"),
+        ];
+
+        public static Image GetImageFromExecutingAssembly(string fileName)
+        {
+            using Stream? stream = Assembly.GetExecutingAssembly().GetManifestResourceStream($"ZombieGameMovement.Sprites.{fileName}");
+            if (stream is not null)
+            {
+                return Image.FromStream(stream);
+            }
+            else
+            {
+                throw new Exception();
+            }
+        }
+    }
+}
