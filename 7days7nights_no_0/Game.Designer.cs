c9531bc9bebb2421bfbf66da8fab3bbbff19da9b
LorincAkos <lorinc.akos13@gmail.com>
2024-04-12 16:58:54 +0200

    Game form modified

--- 7days7nights_no_0/Game.Designer.cs
+++ 7days7nights_no_0/Game.Designer.cs
@@ -3,12 +3,12 @@
     partial class Game
     {
         /// <summary>
-        /// Required designer variable.
+        ///  Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Clean up any resources being used.
+        ///  Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -23,44 +23,90 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Game));
-            pictureBox1 = new PictureBox();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            components = new System.ComponentModel.Container();
+            GameTImer = new System.Windows.Forms.Timer(components);
+            txtammo = new Label();
+            txtscore = new Label();
+            txthealth = new Label();
+            healthBar = new ProgressBar();
             SuspendLayout();
             // 
-            // pictureBox1
+            // GameTImer
             // 
-            pictureBox1.BackgroundImage = (Image)resources.GetObject("pictureBox1.BackgroundImage");
-            pictureBox1.Cursor = Cursors.Cross;
-            pictureBox1.Location = new Point(0, 0);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(2282, 1053);
-            pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
-            pictureBox1.TabIndex = 0;
-            pictureBox1.TabStop = false;
+            GameTImer.Enabled = true;
+            GameTImer.Interval = 20;
+            GameTImer.Tick += MainTimerEvent;
+            // 
+            // txtammo
+            // 
+            txtammo.AutoSize = true;
+            txtammo.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold);
+            txtammo.ForeColor = Color.White;
+            txtammo.Location = new Point(9, 9);
+            txtammo.Name = "txtammo";
+            txtammo.Size = new Size(92, 25);
+            txtammo.TabIndex = 0;
+            txtammo.Text = "Ammo: 0";
+            // 
+            // txtscore
+            // 
+            txtscore.AutoSize = true;
+            txtscore.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold);
+            txtscore.ForeColor = Color.White;
+            txtscore.Location = new Point(403, 8);
+            txtscore.Name = "txtscore";
+            txtscore.Size = new Size(68, 25);
+            txtscore.TabIndex = 1;
+            txtscore.Text = "Kills: 0";
+            // 
+            // txthealth
+            // 
+            txthealth.AutoSize = true;
+            txthealth.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold);
+            txthealth.ForeColor = Color.White;
+            txthealth.Location = new Point(664, 9);
+            txthealth.Name = "txthealth";
+            txthealth.Size = new Size(70, 25);
+            txthealth.TabIndex = 2;
+            txthealth.Text = "Health";
+            // 
+            // healthBar
+            // 
+            healthBar.Location = new Point(743, 14);
+            healthBar.Name = "healthBar";
+            healthBar.Size = new Size(169, 23);
+            healthBar.TabIndex = 3;
+            healthBar.Value = 100;
             // 
             // Game
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1008, 985);
-            Controls.Add(pictureBox1);
-            FormBorderStyle = FormBorderStyle.None;
+            BackColor = Color.FromArgb(64, 64, 64);
+            ClientSize = new Size(1904, 1041);
+            Controls.Add(healthBar);
+            Controls.Add(txthealth);
+            Controls.Add(txtscore);
+            Controls.Add(txtammo);
             Name = "Game";
-            ShowInTaskbar = false;
-            Text = "Game";
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            Text = "Form1";
+            Paint += CharacterPaintEvent;
+            KeyDown += KeyIsDown;
+            KeyUp += KeyIsUp;
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
-
-        private PictureBox pictureBox1;
+        private System.Windows.Forms.Timer GameTImer;
+        private Label txtammo;
+        private Label txtscore;
+        private Label txthealth;
+        private ProgressBar healthBar;
     }
 }
--- 7days7nights_no_0/Game.cs
+++ 7days7nights_no_0/Game.cs
@@ -4,25 +4,314 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static _7days7nights_no_0.Bullet;
+using ZombieGameMovement;
 
 namespace _7days7nights_no_0
 {
     public partial class Game : Form
     {
-        public Game()
+        public static int FormWidth { get; private set; }
+        public static int FormHeight { get; private set; }
+
+        private Player Player { get; set; }
+        private DirectionType Direction { get; set; }
+        private int ZombieSpeed { get; }
+        private int Score { get; set; }
+        private int MaxEnemy { get; set; }
+
+        private int Steps { get; set; }
+        private int SlowDownFrameRate { get; set; }
+
+        private List<Zombie> ZombiesList { get; set; }
+        private List<Bullet> BulletList { get; set; }
+
+
+        public GameWithFixMap()
         {
             InitializeComponent();
+
+            #region Setup
+
+            FormWidth = this.Width;
+            FormHeight = this.Height;
+            DoubleBuffered = true;
+            Player = new(100, 10, FormWidth / 2, FormHeight / 2);
+            ZombieSpeed = 2;
+            Score = 0;
+            Steps = 0;
+            SlowDownFrameRate = 0;
+            ZombiesList = [];
+            BulletList = [];
+            MaxEnemy = 10;
+            this.BackgroundImage = Properties.Resources.FixMap;
+
+            #endregion
+
+            CreateZombie();
         }
 
-        private void Game_paint(object sender, PaintEventArgs e)
+        private void MainTimerEvent(object sender, EventArgs e)
         {
-            Graphics Play = e.Graphics;
+            if (Player.PlayerHealth > 0)
+            {
+                healthBar.Value = Player.PlayerHealth;
+            }
+            else
+            {
+                healthBar.Value = 0;
+                //MessageBox.Show("Ded");
+                GameOver();
+            }
 
-            Play.DrawImage(Image.FromFile(ImageContainer.SkinImage[Convert.ToInt32(Player.Skin.Id)]), Player.x, Player.y, 64, 64);
+            txtscore.Text = "Kills: " + Score;
 
+            if (!Player.Attack)
+            {
+                PlayerMovement();
+            }
+
+            if (ZombiesList.Count < MaxEnemy)
+            {
+                CreateZombie();
+            }
+
+            ZombieMovement();
+            BulletMovement();
+            CheckHit();
+            this.Refresh();
+        }
+
+        private void ZombieMovement()
+        {
+            foreach (Zombie zombie in ZombiesList)
+            {
+                if (zombie.EnemyX > Player.PlayerX)
+                {
+                    zombie.EnemyX -= ZombieSpeed;
+                    zombie.AnimateEnemy(SpriteContainer.zombieWalkRight, zombie);
+                }
+                if (zombie.EnemyX < Player.PlayerX)
+                {
+                    zombie.EnemyX += ZombieSpeed;
+                    zombie.AnimateEnemy(SpriteContainer.zombieWalkRight, zombie);
+                }
+
+                if (zombie.EnemyY > Player.PlayerY)
+                {
+                    zombie.EnemyY -= ZombieSpeed;
+                }
+
+                if (zombie.EnemyY < Player.PlayerY)
+                {
+                    zombie.EnemyY += ZombieSpeed;
+                }
+            }
+        }
+
+        private void PlayerMovement()
+
+        {
+
+            if (Player.GoLeft && Player.PlayerX > 0)
+            {
+                Player.PlayerX -= Player.PlayerSpeed;
+                Player.AnimatePlayer(SpriteContainer.walkRight);
+            }
+
+            if (Player.GoRight && Player.PlayerX + Player.PlayerWidth < this.Width)
+            {
+                Player.PlayerX += Player.PlayerSpeed;
+                Player.AnimatePlayer(SpriteContainer.walkRight);
+            }
+
+            if (Player.GoUp && Player.PlayerY > 150)
+            {
+                Player.PlayerY -= Player.PlayerSpeed;
+            }
+
+            if (Player.GoDown && Player.PlayerY + Player.PlayerHeight < this.Height)
+            {
+                Player.PlayerY += Player.PlayerSpeed;
+            }
+        }
+
+        private void BulletMovement()
+        {
+            if (BulletList.Count == 0)
+            {
+                return;
+            }
+            foreach (Bullet bullet in BulletList)
+            {
+                if (bullet.Direction.Equals(DirectionType.LEFT))
+                {
+                    bullet.BulletImg.Left -= bullet.Speed;
+                }
+
+                else if (bullet.Direction.Equals(DirectionType.RIGHT))
+                {
+                    bullet.BulletImg.Left += bullet.Speed;
+                }
+
+                else if (bullet.Direction.Equals(DirectionType.UP))
+                {
+                    bullet.BulletImg.Top -= bullet.Speed;
+                }
+
+                else if (bullet.Direction.Equals(DirectionType.DOWN))
+                {
+                    bullet.BulletImg.Top += bullet.Speed;
+                }
+
+                else if (bullet.BulletImg.Left < 0 || bullet.BulletImg.Left > GameWithFixMap.FormWidth || bullet.BulletImg.Top < 0 || bullet.BulletImg.Top > GameWithFixMap.FormHeight)
+                {
+                    bullet.BulletTimer.Stop();
+                    bullet.BulletTimer.Dispose();
+                    bullet.BulletImg.Dispose();
+                }
+            }
+        }
+
+        private void CheckHit()
+        {
+            foreach (Zombie zombie in ZombiesList)
+            {
+                //NOTE: Works but it hurts t osee it, try Control Bound Intersects with!!
+                if ((zombie.EnemyX <= Player.PlayerX && zombie.EnemyX + zombie.EnemyWidth >= Player.PlayerX && zombie.EnemyY <= Player.PlayerY && zombie.EnemyY + zombie.EnemyHeight >= Player.PlayerY) ||
+                     (zombie.EnemyX <= Player.PlayerX + Player.PlayerWidth && zombie.EnemyX + zombie.EnemyWidth >= Player.PlayerX + Player.PlayerWidth && zombie.EnemyY <= Player.PlayerY && zombie.EnemyY + zombie.EnemyHeight >= Player.PlayerY) ||
+                    (zombie.EnemyX <= Player.PlayerX && zombie.EnemyX + zombie.EnemyWidth >= Player.PlayerX && zombie.EnemyY <= Player.PlayerY + Player.PlayerHeight && zombie.EnemyY + zombie.EnemyHeight >= Player.PlayerY + Player.PlayerHeight) ||
+                     (zombie.EnemyX <= Player.PlayerX + Player.PlayerWidth && zombie.EnemyX + zombie.EnemyWidth >= Player.PlayerX + Player.PlayerWidth && zombie.EnemyY <= Player.PlayerY + Player.PlayerHeight && zombie.EnemyY + zombie.EnemyHeight >= Player.PlayerY + Player.PlayerHeight)
+                    )
+                {
+                    //NOTE: Program dies if zombie remowed here, do it elsewhere, use diferent logic or a plus parameter
+                    Player.PlayerHealth = Player.PlayerHealth - 25;
+
+                }
+                foreach (Bullet bullet in BulletList)
+                {
+                    if (zombie.EnemyX <= bullet.BulletImg.Left && zombie.EnemyX + zombie.EnemyWidth >= bullet.BulletImg.Left &&
+                        zombie.EnemyY <= bullet.BulletImg.Top && zombie.EnemyY + zombie.EnemyHeight >= bullet.BulletImg.Top)
+                    {
+                        Score++;
+                        //zombie.ZomImage.Dispose();
+                        bullet.BulletImg.Dispose();
+                        ZombiesList.Remove(zombie);
+                        BulletList.Remove(bullet);
+                        return;
+                    }
+                    //if (zombie.EnemyX.Equals(bullet.BulletImg.Left) || zombie.EnemyY.Equals(bullet.BulletImg.Top))
+                    //{
+                    //    Score++;
+                    //    zombie.ZomImage.Dispose();
+                    //    bullet.BulletImg.Dispose();
+                    //    ZombiesList.Remove(zombie);
+                    //    BulletList.Remove(bullet);
+                    //    return;
+                    //}
+                }
+            }
+        }
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode.Equals(Keys.Left))
+            {
+                Player.GoLeft = true;
+                Direction = DirectionType.LEFT;
+            }
+
+            if (e.KeyCode.Equals(Keys.Right))
+            {
+                Player.GoRight = true;
+                Direction = DirectionType.RIGHT;
+            }
+
+            if (e.KeyCode.Equals(Keys.Up))
+            {
+                Player.GoUp = true;
+                Direction = DirectionType.UP;
+            }
+
+            if (e.KeyCode.Equals(Keys.Down))
+            {
+                Player.GoDown = true;
+                Direction = DirectionType.DOWN;
+            }
+
+            if ((e.KeyCode.Equals(Keys.Space)))
+            {
+                Player.Attack = true;
+            }
+
+            if (e.KeyCode.Equals(Keys.K))
+            {
+                this.Close();
+            }
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode.Equals(Keys.Left))
+            {
+                Player.GoLeft = false;
+            }
+
+            if (e.KeyCode.Equals(Keys.Right))
+            {
+                Player.GoRight = false;
+            }
+
+            if (e.KeyCode.Equals(Keys.Up))
+            {
+                Player.GoUp = false;
+            }
+
+            if (e.KeyCode.Equals(Keys.Down))
+            {
+                Player.GoDown = false;
+            }
+
+            if (e.KeyCode.Equals(Keys.Space))
+            {
+                Player.Attack = false;
+                ShootBullet(Direction);
+            }
+        }
+
+        private void ShootBullet(DirectionType direction)
+        {
+            Bullet bullet = new(direction, Player.PlayerX, Player.PlayerY + Player.PlayerHeight / 2);
+            bullet.BulletTimer.Tick += new EventHandler(MainTimerEvent!);
+            BulletList.Add(bullet);
+            bullet.MakeBullet(this);
+        }
+
+        private void CreateZombie()
+        {
+            Zombie zombie = new();
+            ZombiesList.Add(zombie);
+        }
+
+        private void GameOver()
+        {
+            //TODO: Store the important data before closing the form!!!
+            this.Close();
+        }
+
+
+        private void CharacterPaintEvent(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.DrawImage(Player.PlayerImg, Player.PlayerX, Player.PlayerY, Player.PlayerWidth, Player.PlayerHeight);
+            foreach (Zombie zombie in ZombiesList)
+            {
+                g.DrawImage(zombie.ZombieImage, zombie.EnemyX, zombie.EnemyY, zombie.EnemyWidth, zombie.EnemyHeight);
+            }
         }
     }
 }
